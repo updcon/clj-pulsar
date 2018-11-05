7e27b9e121f48b5c3170424fd8471db89e94de3a
MelKori <mieldo@gmail.com>
2018-11-05 20:32:23 +0200

    dummy client+producer+consumer

--- project.clj
+++ project.clj
@@ -2,4 +2,19 @@
   :description "A Clojure library to work with Apache Pulsar"
   :url "https://github.com/updcon/clj-pulsar"
   :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+
+  :main clj-pulsar.core
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.apache.pulsar/pulsar-client "2.2.0"]
+                 [org.apache.pulsar/pulsar-client-admin "2.2.0"]
+
+                 ;; Loggers
+                 ;; -------
+                 [com.taoensso/timbre "4.10.0"]
+                 [com.fzakaria/slf4j-timbre "0.3.12"]
+                 [org.slf4j/slf4j-api "1.7.25"]
+                 [org.slf4j/log4j-over-slf4j "1.7.25"]
+                 [org.slf4j/jul-to-slf4j "1.7.25"]
+                 [org.slf4j/jcl-over-slf4j "1.7.25"]
+                 [net.tbt-post/sentry-tiny "0.1.7"]])
--- src/clj_pulsar/core.clj
+++ src/clj_pulsar/core.clj
@@ -1,3 +1,52 @@
-(ns clj-pulsar.core)
+(ns clj-pulsar.core
+  (:require [taoensso.timbre :as log]
+            [clojure.java.io :as io])
+  (:import (org.apache.pulsar.client.api PulsarClient Producer Consumer)
+           (org.apache.pulsar.client.impl ClientBuilderImpl)
+           (java.util.concurrent TimeUnit)))
 
-(defn -main [& args])
+(defn ^PulsarClient connect [^String url]
+  (-> (ClientBuilderImpl.)
+      (.serviceUrl url)
+      (.build)))
+
+(defn disconnect [^PulsarClient client]
+  (.shutdown client))
+
+(defn ^Producer producer [^PulsarClient client ^String topic]
+  (-> (.newProducer client)
+      (.topic topic)
+      (.create)))
+
+(defn ^Consumer consumer [^PulsarClient client ^String topic ^String group]
+  (-> (.newConsumer client)
+      (.topic (into-array String [topic]))
+      (.subscriptionName group)
+      (.ackTimeout 60 TimeUnit/SECONDS)
+      (.subscribe)))
+
+(defn msg->topic [^Producer p msg]
+  (.send ^Producer p (.getBytes msg)))
+
+(defn msg<-topic [^Consumer c]
+  (when-let [msg (.receive c)]
+    (.acknowledge c msg)
+    (.getData msg)))
+
+(defn -main [& args]
+  (log/info "connecting to:" (first args))
+  (let [url (first args)
+        topic (second args)
+        cl (connect url)]
+    (log/debug "CLIENT:" cl)
+    (with-open [p (producer cl topic)]
+      (log/debug "PRODUCING VIA:" p)
+      (msg->topic p "test")
+      (log/debug "MESSAGE SENT!"))
+    (with-open [c (consumer cl topic "my-group")]
+      (log/debug "CONSUMING WITH:" c)
+      (when-let [msg (msg<-topic c)]
+        (log/debug "MESSAGE TYPE:" (type msg))
+        (log/debug "GOT MESSAGE:" (String. msg))))
+    (log/debug "DISCONNECTING CLIENT:" cl)
+    (disconnect cl)))
